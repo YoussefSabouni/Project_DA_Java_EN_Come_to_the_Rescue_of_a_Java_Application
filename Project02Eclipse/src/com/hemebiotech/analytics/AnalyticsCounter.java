942cfc61c58e3e0bcf11303228313d90d3b90203
Youssef Sabouni <youssefsabouni@icloud.com>
2020-11-05 09:53:23 +0100

    refactor(AnalyticsCounter): Delete Set Interface and use method GetOrDefault of Map Interface.

--- Project02Eclipse/src/com/hemebiotech/analytics/AnalyticsCounter.java
+++ Project02Eclipse/src/com/hemebiotech/analytics/AnalyticsCounter.java
@@ -2,7 +2,10 @@ package com.hemebiotech.analytics;
 
 import java.io.FileNotFoundException;
 import java.io.FileWriter;
-import java.util.*;
+import java.io.IOException;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
 public class AnalyticsCounter {
 
@@ -14,22 +17,22 @@ public class AnalyticsCounter {
             ReadSymptomDataFromFile symptomsFile = new ReadSymptomDataFromFile("symptoms.txt");
 
             // Extracts the list of all symptoms from the file.
-            List<String> symptomsRecovered = symptomsFile.getSymptoms();
-
-            // Deduplication of the list of recovered symptoms.
-            Set<String> symptoms = new HashSet<>(symptomsRecovered);
+            List<String> symptoms = symptomsFile.getSymptoms();
 
             Map<String, Integer> symptomsCounted = new HashMap<>();
 
-            symptoms.forEach(item -> symptomsCounted.put(item, 0));
-
-            // Playback loop for each line of the file.
-            symptomsRecovered.forEach(item -> symptomsCounted.put(item, symptomsCounted.get(item) + 1));
+            // Loop for each symptom of the list.
+            symptoms.forEach(symptom -> symptomsCounted.put(symptom, symptomsCounted.getOrDefault(symptom, 0) + 1));
 
             // Insert count by symptom in the file result.out
-            for (Map.Entry<String, Integer> symptomCounted : symptomsCounted.entrySet()) {
-                writer.write(symptomCounted.getKey() + ": " + symptomCounted.getValue() + "\n");
-            }
+            symptomsCounted.forEach((symptom, count) -> {
+                // I do a try/catch again because it is not "shared" with the lambdas.
+                try {
+                    writer.write(symptom + ": " + count + "\n");
+                } catch (IOException e) {
+                    System.out.println(e.getMessage());
+                }
+            });
 
         } catch (FileNotFoundException e) {
             System.out.println(e.getMessage());
