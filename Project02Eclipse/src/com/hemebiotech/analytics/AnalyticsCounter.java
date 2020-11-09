4b47e9f2c26b67e88a0c6f57c21aae1a5ce84210
Sabouni Youssef <youssefsabouni@icloud.com>
2020-11-09 23:47:36 +0100

    refactor(AnalyticsCounter): Cleaning of the now superfluous try catch and update of JavaDoc

--- Project02Eclipse/src/com/hemebiotech/analytics/AnalyticsCounter.java
+++ Project02Eclipse/src/com/hemebiotech/analytics/AnalyticsCounter.java
@@ -1,45 +1,51 @@
 package com.hemebiotech.analytics;
 
-import java.io.FileNotFoundException;
-import java.io.FileWriter;
 import java.util.List;
 import java.util.Map;
 import java.util.TreeMap;
 
+/**
+ * AnalyticsCouter is the entry point of the program, it retrieves an input .txt file containing side effects (one side
+ * effect per line).
+ * <p>
+ * It sorts these side effects, counts them, and sorts them in alphabetical order. The result is then written to an
+ * output .out file.
+ *
+ * @version 1.1
+ */
 public class AnalyticsCounter {
 
-    public static void main(String[] args) throws Exception {
+    public static void main(String[] args) {
 
-        // Exception handling and resource auto-closure with try-with-resources.
-        try (FileWriter writer = new FileWriter("result.out")) {
+        ISymptomReader symptomsFile = new ReadSymptomDataFromFile("symptoms.txt");
 
-            ISymptomReader symptomsFile = new ReadSymptomDataFromFile("symptoms.txt");
+        // Extracts the list of all symptoms from the file.
+        List<String> symptoms = symptomsFile.getSymptoms();
 
-            // Extracts the list of all symptoms from the file.
-            List<String> symptoms = symptomsFile.getSymptoms();
+        AnalyticsCounter counter = new AnalyticsCounter();
 
-            Map<String, Integer> symptomsCounted = countAndSortSymptoms(symptoms);
+        Map<String, Integer> symptomsCounted = counter.countAndSortSymptoms(symptoms);
 
-            IFileWriter outputFile = new WriteSymptomDataToFile("result.out");
+        IFileWriter outputFile = new WriteSymptomDataToFile("result.out");
 
-            outputFile.writeMapToFile(symptomsCounted);
-
-        } catch (FileNotFoundException e) {
-            System.out.println(e.getMessage());
-        }
+        outputFile.writeMapToFile(symptomsCounted);
 
     }
 
     /**
+     * Retrieve each String from a list, add it to a Map as a key in alphabetical order and add the value 1 to it. If
+     * the value of the key already exists then the value of this key is incremented by 1.
      *
      * @param symptoms
-     * @return
+     *         A list of all symptoms to count and sort
+     *
+     * @return a treemap containing the symptoms with their number of occurrences.
      */
-    private static Map<String, Integer> countAndSortSymptoms(List<String> symptoms) {
+    private Map<String, Integer> countAndSortSymptoms(List<String> symptoms) {
 
         Map<String, Integer> symptomsCounted = new TreeMap<>();
 
-        // Loop for each symptom of the list.
+        // Creates a key in symptomsCounted and increments it by 1 at each occurrence..
         symptoms.forEach(symptom -> symptomsCounted.put(symptom, symptomsCounted.getOrDefault(symptom, 0) + 1));
 
         return symptomsCounted;
