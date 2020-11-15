b9768a086aec7da45e920e081d075f0d87fedefb
Sabouni Youssef <youssefsabouni@icloud.com>
2020-11-15 21:04:53 +0100

    refactor(AnalyticCounter): Separation of list management methods in another class

--- Project02Eclipse/src/com/hemebiotech/analytics/AnalyticsCounter.java
+++ Project02Eclipse/src/com/hemebiotech/analytics/AnalyticsCounter.java
@@ -2,11 +2,13 @@ package com.hemebiotech.analytics;
 
 import com.hemebiotech.analytics.interfaces.IFileWriter;
 import com.hemebiotech.analytics.interfaces.ISymptomReader;
+import com.hemebiotech.analytics.tools.ReadSymptomDataFromFile;
+import com.hemebiotech.analytics.tools.SymptomsManagement;
+import com.hemebiotech.analytics.tools.WriteSymptomDataToFile;
 
-import java.util.Collections;
-import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
+
 
 /**
  * AnalyticsCouter is the entry point of the program, it retrieves an input .txt file containing side effects (one side
@@ -60,43 +62,15 @@ public class AnalyticsCounter {
     }
 
     /**
-     * Retrieve each {@link String} from a {@link List}, add it to a {@link Map} as a key in alphabetical order and add
-     * the value 1 to it. If the value of the key already exists then the value of this key is incremented by 1.
-     *
-     * @param symptoms
-     *         A list of all symptoms to count and sort
-     *
-     * @return a treemap containing the symptoms with their number of occurrences.
-     */
-    private static Map<String, Integer> countAndSortSymptoms(List<String> symptoms) {
-
-        // keys in TreeMap are sorted using their natural order
-        Map<String, Integer> symptomsCounted = new HashMap<>();
-
-        // Creates a key in symptomsCounted and increments it by 1 at each occurrence..
-        symptoms.forEach(symptom -> symptomsCounted.put(symptom, symptomsCounted.getOrDefault(symptom, 0) + 1));
-
-        return symptomsCounted;
-    }
-
-
-    public List<String> listSorter(List<String> list) {
-
-        Collections.sort(list);
-
-        return list;
-    }
-
-    /**
      * Executes the count analysis process
      */
     public void run() {
 
         List<String> symptoms = this.SYMPTOMS_READER.getSymptoms();
 
-        symptoms = listSorter(symptoms);
+        Map<String, Integer> symptomsCounted = SymptomsManagement.countSymptoms(symptoms);
 
-        Map<String, Integer> symptomsCounted = AnalyticsCounter.countAndSortSymptoms(symptoms);
+        symptomsCounted = SymptomsManagement.sortSymptoms(symptomsCounted);
 
         this.OUTPUT_WRITER.writeMapToFile(symptomsCounted);
     }
